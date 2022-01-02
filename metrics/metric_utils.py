8601a034e817161becc8dfaeee8f76bca8910508
nupurkmr9 <nupurkmr9@gmail.com>
2022-01-02 12:17:02 -0500

    metric

--- metrics/metric_utils.py
+++ metrics/metric_utils.py
@@ -19,7 +19,7 @@ import dnnlib
 #----------------------------------------------------------------------------
 
 class MetricOptions:
-    def __init__(self, G=None, G_kwargs={}, dataset_kwargs={}, num_gpus=1, rank=0, device=None, progress=None, cache=True,clean=True, num_gen=50000, ):
+    def __init__(self, G=None, G_kwargs={}, dataset_kwargs={}, num_gpus=1, rank=0, device=None, progress=None, cache=True, clean=False, num_gen=50000, ):
         assert 0 <= rank < num_gpus
         self.G              = G
         self.G_kwargs       = dnnlib.EasyDict(G_kwargs)
